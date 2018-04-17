b2cc6ab4315101ae235817a1dc12cc1bc548a664
Mike Steward <mike@stew.cloud>
2018-04-17 14:16:18 -0400

    updates to header

--- open_buckets.py
+++ open_buckets.py
@@ -9,21 +9,6 @@ and then message the bucket policy, if found, to an SNS topic that administrator
 can subscribe to.
 
 """
-<<<<<<< HEAD
-=======
-# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"). You may not
-# use this file except in compliance with the License. A copy of the License
-# is located at
-#
-#     http://aws.amazon.com/apache2.0/
-#
-# or in the "license" file accompanying this file. This file is distributed on
-# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-# express or implied. See the License for the specific language governing
-# permissions and limitations under the License.
->>>>>>> 40323cc7740e2de2b8eed6a4642b956c55a0a02b
 import boto3
 from botocore.exceptions import ClientError
 import json
